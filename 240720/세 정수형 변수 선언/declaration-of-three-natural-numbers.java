29ac427ef0bf082ed46712864619522fc294168f
wjxor <shko940575@gmail.com>
2024-07-20 17:21:57 +0900

    Title: 세 정수형 변수 선언, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240720/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 23;
+        int num3 = 30;
+
+        System.out.printf("%d + %d = %d", num1, num2, num3);
+    }
+}

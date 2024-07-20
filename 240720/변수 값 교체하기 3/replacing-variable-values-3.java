700452ead18e90f9c25ae04faf0bf56a2d1e8aea
wjxor <shko940575@gmail.com>
2024-07-20 17:52:19 +0900

    Title: 변수 값 교체하기 3, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240720/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int temp = b;
+
+        b = a;
+        a = temp;
+
+        System.out.println(a + "\n" + b);
+    }
+}

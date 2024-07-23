7112b38df596e9c5605f6b659a6e8bc73824273c
wjxor <shko940575@gmail.com>
2024-07-23 20:51:22 +0900

    Title: 합과 평균의 차, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240723/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = sc.nextInt() + sc.nextInt() + sc.nextInt();
+
+        System.out.printf("%d\n%d\n%d", sum, (sum / 3), (sum - (sum / 3)));
+
+        sc.close();
+    }
+}

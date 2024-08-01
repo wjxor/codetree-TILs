86315b1976c0df59ce326f828c98bf533b29f042
wjxor <shko940575@gmail.com>
2024-08-01 21:37:12 +0900

    Title: a/b 출력, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240801/a/b 출력/a-divide-b.java
+++ 240801/a/b 출력/a-divide-b.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+        System.out.print(a / b + ".");
         
-        System.out.printf("%22.20f", (double)a / b);
-
-        sc.close();
+        a %= b;
+        for(int i = 0; i < 20; i++) {
+            a *= 10;
+            System.out.print(a / b);
+            a %= b;
+        }
     }
 }

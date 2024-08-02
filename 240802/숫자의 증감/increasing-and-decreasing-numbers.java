98c3cc3b6fd4aab5eda67f1dc84ad745cb1fda43
wjxor <shko940575@gmail.com>
2024-08-02 20:18:31 +0900

    Title: 숫자의 증감, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240802/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        char c = sc.next().charAt(0);
+        int n= sc.nextInt();
+
+        if (c == 'A') {
+            for (int i = 1; i <= n; i++) {
+                System.out.print(i + " ");
+            }
+        } else if (c == 'D') {
+            for (int j = n; j >= 1; j--) {
+                System.out.print(j + " ");
+                
+            }
+        } else
+            return;
+
+        sc.close();
+
+    }
+}

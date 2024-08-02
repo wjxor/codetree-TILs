4555f54bb307a210289e05ab7ad85e994431b738
wjxor <shko940575@gmail.com>
2024-08-02 23:51:20 +0900

    Title: a 부터 b 까지, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240802/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = 0;
+        // a에서 시작해서 조건에 따라 커지다가 b보다 커지는 순간에 종료
+        // 홀수->2배
+        // 짝수->+3
+
+        System.out.print(a + " ");
+        for (int i = a; i < b;) {
+            if (i % 2 == 0)
+                i += 3;
+            else
+                i *= 2;
+            
+            System.out.print(i + " ");
+        }  
+
+        sc.close();
+    }
+}

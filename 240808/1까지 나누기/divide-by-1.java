4a24286148600bb6e713aece73cb0ed51592e0d7
wjxor <shko940575@gmail.com>
2024-08-08 07:39:30 +0900

    Title: 1까지 나누기, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240808/1까지 나누기/divide-by-1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; ; i++) {
+            n /= i;
+            cnt++;
+
+            if (n <= 1) {
+                System.out.println(cnt);
+                break;
+            }
+        }
+
+        
+
+        sc.close();
+    }
+
+
+}

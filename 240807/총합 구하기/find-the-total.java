0a72d9b8cdeb57e85d4d71e93d3c33164d346700
wjxor <shko940575@gmail.com>
2024-08-07 21:55:44 +0900

    Title: 총합 구하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240807/총합 구하기/find-the-total.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 6 == 0 && i % 8 != 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+
+
+}

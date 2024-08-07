2bfb2d61a779f6c537f05c180a007e777feadd13
wjxor <shko940575@gmail.com>
2024-08-07 20:51:11 +0900

    Title: 완전수 판별기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240807/완전수 판별기/perfect-number-discriminator.java
+++ 240807/완전수 판별기/perfect-number-discriminator.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         int sum = 0;
 
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < n; i++) {
             if (n % i == 0) {
                 sum += i;
             }

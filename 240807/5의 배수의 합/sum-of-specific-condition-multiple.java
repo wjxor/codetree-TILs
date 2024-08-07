fb3dc9cedeacfce9ff8a6f41f4b0c16d1614a02d
wjxor <shko940575@gmail.com>
2024-08-07 20:37:30 +0900

    Title: 5의 배수의 합, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240807/5의 배수의 합/sum-of-specific-condition-multiple.java
+++ 240807/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = 0;
@@ -12,9 +12,11 @@ public class Main {
         if (a > b) {
             max = a;
             min = b;
-        } else
+        } else {
             max = b;
             min = a;
+        }
+
 
         for (int i = min; i <= max; i++) {
             if (i % 5 == 0) {

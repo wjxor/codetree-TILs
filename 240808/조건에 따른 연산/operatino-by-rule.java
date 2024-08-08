8c6081ad468cf49b667a68a54a9cc0e1b8b8174e
wjxor <shko940575@gmail.com>
2024-08-08 21:16:47 +0900

    Title: 조건에 따른 연산, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240808/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int cnt = 0;
+
+        while (n <= 1000) {
+            if (n % 2 == 0) {
+                n = n * 3 + 1;
+                cnt++;
+            } else {
+                n = n * 2 + 2;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+        
+        scanner.close();
+    }
+}

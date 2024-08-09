d26eb5771389374c54ab056c03b2deba924044c8
wjxor <shko940575@gmail.com>
2024-08-09 21:40:07 +0900

    Title: 모두 3의 배수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240809/모두 3의 배수/all-multiples-of-3.java
+++ 240809/모두 3의 배수/all-multiples-of-3.java
@@ -9,7 +9,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             int num = sc.nextInt();
 
-            if (num % i != 0) {
+            if (num % 3 != 0) {
                 existed = false;
             }
         }
